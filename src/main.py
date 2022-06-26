4aed52f8b52f1cae5cb722f7fbbf7d82c74712b6
kevinlul <6320810+kevinlul@users.noreply.github.com>
2022-06-26 18:34:00 -0400

    Refactor helper functions for reuse

--- src/main.py
+++ src/main.py
@@ -1,46 +1,11 @@
 import random
 import sys
 from time import sleep
-from typing import Optional
 
 import httpx
 from ruamel.yaml import YAML
-from ruamel.yaml.scalarstring import LiteralScalarString
 
-
-def get_retry(client: httpx.Client, url: str) -> httpx.Response:
-    for retry in range(5):
-        try:
-            return client.get(url, follow_redirects=True)
-        except httpx.RequestError as e:
-            print(f"TRY {retry}\t{e}\t{url}", flush=True)
-            sleep(random.uniform(1 + retry, 2 + retry))
-
-
-def download(client: httpx.Client, yaml: YAML, url: str) -> Optional[str]:
-    response = client.get(url, follow_redirects=True)
-    response.raise_for_status()
-    result = response.json()
-    if not result.get("batchcomplete"):
-        print(f"batchcomplete != true | {url}", flush=True)
-    gcmcontinue = result["continue"]["gcmcontinue"] if result.get("continue") else None
-    for page in result["query"]["pages"]:
-        pageid = page["pageid"]
-        n_revisions = len(page["revisions"])
-        contentformat = page["revisions"][0]["contentformat"]
-        contentmodel = page["revisions"][0]["contentmodel"]
-        if n_revisions != 1:
-            print(f"{n_revisions} revisions | {pageid} | {url}", flush=True)
-        if contentformat != "text/x-wiki":
-            print(f"contentformat = {contentformat} | {pageid} | {url}", flush=True)
-        if contentmodel != "wikitext":
-            print(f"contentmodel = {contentmodel} | {pageid} | {url}", flush=True)
-        with open(f"{pageid}.yaml", mode="w", encoding="utf-8") as out:
-            yaml.dump({
-                "title": page["title"],
-                "wikitext": LiteralScalarString(page["revisions"][0]["content"])
-            }, out)
-    return gcmcontinue
+from utils import download
 
 
 def main():
@@ -50,12 +15,12 @@ def main():
         if len(sys.argv) > 1:
             gcmcontinue = sys.argv[1]
         else:
-            gcmcontinue = download(client, yaml, url)
+            gcmcontinue = download(client, yaml, "gcmcontinue", url)
         while gcmcontinue is not None:
             print(f"gcmcontinue = {gcmcontinue}", flush=True)
             sleep(random.uniform(1, 2))
             gcmurl = f"{url}&gcmcontinue={gcmcontinue}"
-            gcmcontinue = download(client, yaml, gcmurl)
+            gcmcontinue = download(client, yaml, "gcmcontinue", gcmurl)
 
 
 if __name__ == "__main__":
---
+++ src/utils.py
@@ -0,0 +1,42 @@
+import random
+from time import sleep
+from typing import Optional
+
+import httpx
+from ruamel.yaml import YAML
+from ruamel.yaml.scalarstring import LiteralScalarString
+
+
+def get_retry(client: httpx.Client, url: str) -> httpx.Response:
+    for retry in range(5):
+        try:
+            return client.get(url, follow_redirects=True)
+        except httpx.RequestError as e:
+            print(f"TRY {retry}\t{e}\t{url}", flush=True)
+            sleep(random.uniform(1 + retry, 2 + retry))
+
+
+def download(client: httpx.Client, yaml: YAML, continue_key: str, url: str) -> Optional[str]:
+    response = client.get(url, follow_redirects=True)
+    response.raise_for_status()
+    result = response.json()
+    if not result.get("batchcomplete"):
+        print(f"batchcomplete != true | {url}", flush=True)
+    continue_value = result["continue"][continue_key] if result.get("continue") else None
+    for page in result["query"]["pages"]:
+        pageid = page["pageid"]
+        n_revisions = len(page["revisions"])
+        contentformat = page["revisions"][0]["contentformat"]
+        contentmodel = page["revisions"][0]["contentmodel"]
+        if n_revisions != 1:
+            print(f"{n_revisions} revisions | {pageid} | {url}", flush=True)
+        if contentformat != "text/x-wiki":
+            print(f"contentformat = {contentformat} | {pageid} | {url}", flush=True)
+        if contentmodel != "wikitext":
+            print(f"contentmodel = {contentmodel} | {pageid} | {url}", flush=True)
+        with open(f"{pageid}.yaml", mode="w", encoding="utf-8") as out:
+            yaml.dump({
+                "title": page["title"],
+                "wikitext": LiteralScalarString(page["revisions"][0]["content"])
+            }, out)
+    return continue_value
